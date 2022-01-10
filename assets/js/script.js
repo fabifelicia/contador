ec64271ee9c039e4e723e2fd6d814ade1af9585a
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-01-10 11:51:22 -0300

    modified styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,10 @@ function increment() {
 function decrement() {
     currentNumber--;
     currentNumberWrapper.innerHTML = currentNumber;
+    //modificando cor quando numero for negativo
+    
 }
+
+//addEventListener
+//Crie condicionais que desabilitam os botoes entre 0 e 10
+//Mude a cor do texto para vermelho quando o numero for negativo
