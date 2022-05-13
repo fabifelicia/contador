a013e270dbd607bcbe866797e181f73ab2b810ff
fabifelicia <fabianabarretomenezes@gmail.com>
2022-05-13 11:53:35 -0300

    add modules

---
+++ modules/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs'
+
+console.log(mostraIdade('Fabiana', 40))
+console.log(mostraCidade('Fabiana', 'Aracaju'))
+console.log(mostraHobby('Fabiana', 'ler livros'))
